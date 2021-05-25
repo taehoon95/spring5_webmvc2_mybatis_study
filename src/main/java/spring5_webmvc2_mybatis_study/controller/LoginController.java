7bfe5c41f1dacd16cdaa65327dd338eb42c4adc1
taehoon95 <leeth950304@gmail.com>
2021-05-25 16:49:15 +0900

    인터셉터(로그인은 안했는데 로그인후 들어갈 페이지를 주소창에 입력했을때 로그인페이지로 이동)

--- src/main/java/spring5_webmvc2_mybatis_study/controller/LoginController.java
+++ src/main/java/spring5_webmvc2_mybatis_study/controller/LoginController.java
@@ -1,11 +1,14 @@
 package spring5_webmvc2_mybatis_study.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,25 +18,40 @@ import spring5_webmvc2_mybatis_study.dto.LoginCommand;
 import spring5_webmvc2_mybatis_study.exception.WrongIdPasswordException;
 import spring5_webmvc2_mybatis_study.service.AuthService;
 
-@Configuration
+@Controller
 @RequestMapping("/login")
 public class LoginController {
 	@Autowired
 	private AuthService authService;
 	
 	@GetMapping
-	public String form(LoginCommand loginCommand) {
+	public String form(LoginCommand loginCommand, @CookieValue(value = "REMEMBER", required = false) Cookie rCookie) {
+		if(rCookie != null) {
+			loginCommand.setEmail(rCookie.getValue());
+			loginCommand.setRememberEmail(true);
+		}
 		return "/login/loginForm";
 	}
 	
 	@PostMapping
-	public String submit(@Valid LoginCommand loginCommand, Errors errors, HttpSession session) {
+	public String submit(@Valid LoginCommand loginCommand, Errors errors, HttpSession session, HttpServletResponse response) {
 		if(errors.hasErrors()) {
 			return "/login/loginForm";
 		}
 		try {
 			AuthInfo authInfo = authService.authenicate(loginCommand.getEmail(), loginCommand.getPassword());
 			session.setAttribute("authInfo", authInfo);
+			
+			Cookie rememberCookie = new Cookie("REMEMBER", loginCommand.getEmail());
+			rememberCookie.setPath("/");
+			if(loginCommand.isRememberEmail()) {
+				rememberCookie.setMaxAge(60*60*24*30);
+			}else {
+				rememberCookie.setMaxAge(0);
+			}
+			response.addCookie(rememberCookie);
+			
+			
 			return "login/loginSuccess";
 		}catch (WrongIdPasswordException e) {
 			errors.reject("idPasswordNotMatching");
