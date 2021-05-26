bc73b52d754af3c2441b85cf693525aabe45073b
taehoon95 <leeth950304@gmail.com>
2021-05-26 10:46:08 +0900

    @ControllerAdvice,@ExceptionHandler로 공통 에러처리

--- src/main/java/spring5_webmvc2_mybatis_study/common/CommonExceptionHandler.java
+++ src/main/java/spring5_webmvc2_mybatis_study/common/CommonExceptionHandler.java
@@ -1,9 +1,9 @@
-package spring5_webmvc_study.common;
+package spring5_webmvc2_mybatis_study.common;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice("spring5_webmvc_study")
+@ControllerAdvice("spring5_webmvc2_mybatis_study")
 public class CommonExceptionHandler {
 	@ExceptionHandler(RuntimeException.class)
 	public String handleRuntimeException() {
